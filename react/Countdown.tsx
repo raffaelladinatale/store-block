37a57e8a0836360263489218ebbdc2774982b61f
rdinatale <raffaella.dinatale@filoblu.com>
2020-04-30 15:57:39 +0200

    title component

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -2,20 +2,16 @@ import React, { useState } from 'react';
 import { TimeSplit } from './typings/global';
 import { tick } from './utils/time';
 import { useCssHandles } from "vtex.css-handles";
-import { FormattedMessage } from 'react-intl';
-
 
 interface CountdownProps {
-  title: string,
   targetDate: string
 }
 
 
 const DEFAULT_TARGET_DATE = (new Date('2020-06-25')).toISOString();
-const CSS_HANDLES = ["container","countdown", "title"]
+const CSS_HANDLES = ["countdown"]
 
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ 
-  title,
   targetDate = DEFAULT_TARGET_DATE 
 }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
@@ -24,8 +20,6 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
     seconds: '00'
   })
 
-  const titleText = title || <FormattedMessage id="countdown.title" />
-
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
@@ -33,7 +27,6 @@ const Countdown: StorefrontFunctionComponent<CountdownProps> = ({
   return (
     <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
       <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-        <div className={`${handles.title} db tc`}>{titleText}</div>
       <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
     </div>
   </div>
@@ -46,11 +39,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-      title: 'I am a title',
-      type: 'string',
-      default: null,
-    },
     targetDate: {
       title: 'Final Date',
       description: 'Final date used in the countdown',
